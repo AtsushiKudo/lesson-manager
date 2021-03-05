7be42b665383373786f315f7458e6d95a9512044
java3pm <java3pm@PCJAVA3PM01>
2021-03-05 20:55:34 +0900

    edit sidebar

--- LessonManagement/src/main/java/com/example/LessonManagement/controller/LessonController.java
+++ LessonManagement/src/main/java/com/example/LessonManagement/controller/LessonController.java
@@ -1,10 +1,12 @@
 package com.example.LessonManagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.LessonManagement.model.Lesson;
@@ -32,6 +34,32 @@ public class LessonController {
         }
         lessonRepository.save(lesson);
         return "redirect:/?lesson_register";
+    }
+
+
+
+    @GetMapping("/admin/lesson/index")
+    public String showList(Model model) {
+        model.addAttribute("lessons", lessonRepository.findAll());
+        return "lesson/lesson-index";
+    }
+
+    @GetMapping("/admin/lesson/edit/{id}")
+    public String editLesson(@PathVariable Long id, Model model) {
+        model.addAttribute("lesson", lessonRepository.findById(id));
+        return "/lesson/lesson-form";
+    }
+
+    @GetMapping("/admin/lesson/delete/{id}")
+    public String deleteLesson(@PathVariable Long id) {
+        lessonRepository.deleteById(id);
+        return "redirect:/?lesson_delete";
+    }
+
+    @GetMapping("/admin/hold-lesson/add/{id}")
+    public String addHoldLesson(@PathVariable Long id, Model model) {
+
+        return "/lesson/hold-lesson-form";
     }
 
 }
